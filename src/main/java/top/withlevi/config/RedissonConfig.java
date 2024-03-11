05e9bf772d27dc5d6c89efef10f90671fd139780
withLevi <zengyin.ssr@gmail.com>
2024-03-11 22:51:39 +0800

    Add Redis limter manager.

---
+++ src/main/java/top/withlevi/config/RedissonConfig.java
@@ -0,0 +1,41 @@
+package top.withlevi.config;
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
+public class RedissonConfig {
+
+
+    private Integer database;
+
+    private String host;
+
+    private Integer port;
+
+    private String password;
+
+
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config =  new Config();
+        config.useSingleServer()
+                .setDatabase(database)
+                .setAddress("redis://"+host+":"+port)
+                //.setAddress("redis://43.138.11.203:6380")
+                .setPassword(password);
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+
+    }
+
+}
