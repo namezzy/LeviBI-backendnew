968afb7b83996b65650efe6abc9f449251bb7ec9
withLevi <zengyin.ssr@gmail.com>
2024-03-11 17:54:17 +0800

    Add queryChartDataMap- Initial realization of sub-library and sub-table

--- src/main/java/top/withlevi/mapper/ChartMapper.java
+++ src/main/java/top/withlevi/mapper/ChartMapper.java
@@ -4,6 +4,9 @@ package top.withlevi.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import top.withlevi.model.entity.Chart;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Hello World
  * @description 针对表【chart(图表信息表)】的数据库操作Mapper
@@ -11,6 +14,17 @@ import top.withlevi.model.entity.Chart;
  * @Entity generator.domain.Chart
  */
 public interface ChartMapper extends BaseMapper<Chart> {
+
+    /**
+     * 方法的返回类型是  List<Map<String, Object>>
+     *  表示返回的是一个由多个map组合成的集合，每个map代表一行查询结果
+     *  并将其封装成了一组键值对形式的对象, 其中String类型代表了键的类型为字符串
+     *  Object 类型代表了值的类型为任意对象， 使得这个方法可以适应不同类型的数据查询
+     * @param querySql
+     * @return
+     */
+
+    List<Map<String, Object>> queryChartData(String querySql);
 
 }
 
